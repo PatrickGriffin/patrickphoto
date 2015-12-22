5f3c83ac57c45ad484b48ce85316db2d6a3a8be7
PatrickGriffin <patrickgriffin1409@gmail.com>
2015-12-22 01:52:17 +0000

    changes to production.py again again again197

--- kevingriffinphoto/settings/production.py
+++ kevingriffinphoto/settings/production.py
@@ -15,6 +15,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Static asset configuration
 # import os
