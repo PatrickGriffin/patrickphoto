dd5266bd9ed2da6dc49c8bf723262b2bcbc2fda4
PatrickGriffin <patrickgriffin1409@gmail.com>
2015-12-31 00:04:51 +0000

    changed Template Debug

--- kevingriffinphoto/urls.py
+++ kevingriffinphoto/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     url(r'^blog/', 'portfolio.views.blog', name='blog'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-print(settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
