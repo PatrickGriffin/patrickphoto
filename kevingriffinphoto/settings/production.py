57b76bc9591719b832f6a93315c6db978f32b805
PatrickGriffin <patrickgriffin1409@gmail.com>
2016-01-03 13:43:59 +0000

    removed a unneccasry file

--- kevingriffinphoto/settings/production.py
+++ kevingriffinphoto/settings/production.py
@@ -11,7 +11,6 @@ DATABASES = settings.DATABASES
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
-print(INSTALLED_APPS)
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
